63469f04c4c168d9b171322c5e309875e315834c
Philip Harrison <philip@mailharrison.com>
2013-09-11 11:02:28 +0100

    Initial

---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: './',
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      // Vendor
+      'components/bower-angular/angular.js',
+      'components/bower-angular-mocks/angular-mocks.js',
+      'components/bower-lodash-angular/lodash.custom.js',
+      'components/bower-redefine/redefine.js',
+      'components/ng-http-factory/ng-http-factory.js',
+
+      // Specs helpers
+      'components/jasmine-helpers/*.js',
+
+      'ng-gc-user-api-service*.js'
+    ],
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    frameworks: ['jasmine'],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters: ['dots', 'growl'],
+
+
+    reportSlowerThan: 50,
+
+
+    // enable / disable watching file and executing tests
+    // whenever any file changes
+    autoWatch: true,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+# GoCardless User Api Service
+
+![](https://circleci.com/gh/gocardless-ng/ng-gc-user-api-service.png?circle-token=:circle-token)
+
+## Copyright and license
+
+Copyright 2013 GoCardless, Inc under the MIT license.
